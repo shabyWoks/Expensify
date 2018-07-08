14d814380bb4dd8f100b5ec81273620effa886d1
shabyWoks <shaby1@outlook.com>
2018-07-09 00:43:41 +0530

    Formatted a file

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment'; // Handling and formatting time and date
+import numeral from 'numeral'; // formatting currency
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>
+      {numeral(amount/100).format('£0,0.00')} 
+      - 
+      {moment(createdAt).format('MMMM Do, YYYY')}</p>
   </div>
 );
 
